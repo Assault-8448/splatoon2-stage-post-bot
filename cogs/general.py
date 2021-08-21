cd801c06b377cc5fa81b6dbaf9e7f4ca2b820c26
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-22 05:12:27 +0900

    とりあえずjsonそのまま返すようにした

--- cogs/general.py
+++ cogs/general.py
@@ -6,8 +6,13 @@ class General(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def say(self, ctx, *message):
-        await ctx.send(' '.join(message))
+    async def guide(self, ctx):
+        embed = discord.Embed(title="つかいかた",
+                              description="``;map``コマンドを叩くだけで全部出てきます。",
+                              color=0x333333)
+        embed.set_footer(text="APIは毎度叩いているので連投はお控え下さい")
+
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(General(bot))
--- cogs/splatoon2.py
+++ cogs/splatoon2.py
@@ -1,13 +1,21 @@
 from discord.ext import commands
 import discord as discord
 
+import requests
+import json
+
 class Splatoon2(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def test(self, ctx):
-        await ctx.send("唐澤貴洋殺す")
+    async def regular(self, ctx):
+
+        url = "https://spla2.yuu26.com/regular/now"
+        response = requests.get(url)
+        jsonData = response.json()
+
+        await ctx.send(jsonData["result"])
 
 def setup(bot):
     bot.add_cog(Splatoon2(bot))
