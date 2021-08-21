28f01e38c637d922407dc36d9b99cd4b045c5dc2
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-22 05:21:16 +0900

    ルール吐き出せるようになった（これからマップも吐き出すようにする

--- cogs/splatoon2.py
+++ cogs/splatoon2.py
@@ -12,10 +12,12 @@ class Splatoon2(commands.Cog):
     async def regular(self, ctx):
 
         url = "https://spla2.yuu26.com/regular/now"
+        ua = "Splatoon2MapPostBot/WIP (twitter @SzlyNe_, Discord Assault#6639"
+        headers = {"User-Agent": ua}
         response = requests.get(url)
         jsonData = response.json()
 
-        await ctx.send(jsonData["result"])
+        await ctx.send(jsonData["result"][0]["rule"])
 
 def setup(bot):
     bot.add_cog(Splatoon2(bot))
