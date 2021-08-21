4acbcc868ed73f80713e7a7061f28a4273b3dae7
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-22 03:07:39 +0900

    app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+import asyncio
+import traceback
+import os
+import yaml
+
+import discord
+import discord.ext.commands as cmd
+
+COGS = [
+    "cogs.general",
+    "cogs.splatoon2"
+]
+
+class Bot(cmd.Bot):
+    def __init__(self, prefix):
+        super().__init__(command_prefix=prefix)
+
+        for cog in COGS:
+            try:
+                self.load_extension(cog)
+            except Exception as e:
+                print(f"Failed to load extension {cog}.")
+                traceback.print_exc()
+
+    async def on_ready(self):
+        print(f"Logged in as: {self.user.name}, {self.user.id}")
+
+        game = discord.Game(";help")
+        await self.change_presence(status=discord.Status.online, activity=game)
+
+default_token_data = {
+        "using": "main",
+        "main": "<Paste your bot token here>"
+}
+
+def main():
+    if not os.path.exists("token.yml"):
+        with open("token.yml", "w") as f:
+            yaml.dump(default_token_data, f)
+            print(
+                """
+                Edit token.yml to add your bot token.
+                in default, it will secelted `main`.
+                """
+            )
+            exit()
+    else:
+        with open("token.yml", "r") as f:
+            data = yaml.safe_load(f)
+            token_key = data["using"]
+            token = data[token_key]
+
+        bot = Bot(prefix=f';')
+        bot.run(token)
+
+if __name__ == "__main__":
+    main()
