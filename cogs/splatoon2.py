803f0f90578c7a9577417f0a9906503ecffd0a9c
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-23 00:52:35 +0900

    サーモンランも出力するようにした

--- cogs/splatoon2.py
+++ cogs/splatoon2.py
@@ -35,7 +35,9 @@ class Splatoon2(commands.Cog):
                                     title = "レギュラーマッチ（ナワバリバトル）",
                                     color=0x00ff00,
                                     description=f"{dt.strftime('%Y年%m月%d日 %H:%M:%S')}時点と次のステージ情報")
-        regular_embed.add_field(name=f"いま: **{regular_map0}** / **{regular_map1}**", value=f"つぎ: **{regular_nextmap0}** / **{regular_nextmap1}**", inline=False)
+        regular_embed.add_field(name=f"いま: **{regular_map0}** / **{regular_map1}**",
+                                value=f"つぎ: **{regular_nextmap0}** / **{regular_nextmap1}**",
+                                inline=False)
         regular_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/4/4c/Mode_Icon_Regular_Battle_2.png")
         regular_embed.set_footer(text="API: https://spla2.yuu26.com")
 
@@ -68,7 +70,9 @@ class Splatoon2(commands.Cog):
                                     color=0xff0000,
                                     description=f"{dt.strftime('%Y年%m月%d日 %H:%M:%S')}時点と次のモードとステージ情報")
         ranked_embed.add_field(name=f"いま: **{ranked_mode}**", value=f"つぎ: **{ranked_nextmode}**", inline=False)
-        ranked_embed.add_field(name=f"いま: **{ranked_map0}** / **{ranked_map1}**", value=f"つぎ: **{ranked_nextmap0}** / **{ranked_nextmap1}**", inline=False)
+        ranked_embed.add_field(name=f"いま: **{ranked_map0}** / **{ranked_map1}**",
+                              value=f"つぎ: **{ranked_nextmap0}** / **{ranked_nextmap1}**",
+                              inline=False)
         ranked_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/2/2c/Mode_Icon_Ranked_Battle_2.png")
         ranked_embed.set_footer(text="API: https://spla2.yuu26.com")
 
@@ -101,11 +105,43 @@ class Splatoon2(commands.Cog):
                                     color=0xffc0cb,
                                     description=f"{dt.strftime('%Y年%m月%d日 %H:%M:%S')}時点と次のモードとステージ情報")
         league_embed.add_field(name=f"いま: **{league_mode}**", value=f"つぎ: **{league_nextmode}**", inline=False)
-        league_embed.add_field(name=f"いま: **{league_map0}** / **{league_map1}**", value=f"つぎ: **{league_nextmap0}** / **{league_nextmap1}**", inline=False)
+        league_embed.add_field(name=f"いま: **{league_map0}** / **{league_map1}**",
+                               value=f"つぎ: **{league_nextmap0}** / **{league_nextmap1}**",
+                               inline=False)
         league_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/9/9b/Symbol_LeagueF.png")
         league_embed.set_footer(text="API: https://spla2.yuu26.com")
 
         await ctx.send(embed=league_embed)
+
+    @commands.command()
+    async def salmon(self, ctx):
+
+        url = "https://spla2.yuu26.com/coop/schedule"
+        ua = "Splatoon2MapPostBot_regular/WIP (twitter @SzlyNe_, Discord Assault#6639"
+        headers = {"User-Agent": ua}
+
+        response = requests.get(url)
+        jsonData = response.json()
+
+        salmon_map = jsonData["result"][0]["stage"]["name"]
+        salmon_wpn_1 = jsonData["result"][0]["weapons"][0]["name"]
+        salmon_wpn_2 = jsonData["result"][0]["weapons"][1]["name"]
+        salmon_wpn_3 = jsonData["result"][0]["weapons"][2]["name"]
+        salmon_wpn_4 = jsonData["result"][0]["weapons"][3]["name"]
+
+        salmon_logo  = jsonData["result"][0]["stage"]["image"]
+
+        salmon_embed = discord.Embed(
+                                    title = "サーモンラン",
+                                    color = 0xff7f50,
+                                    description=f"{dt.strftime('%Y年%m月%d日 %H:%M:%S')}時点のブキとステージ情報")
+        salmon_embed.add_field(name=f"ブキ: {salmon_wpn_1}, {salmon_wpn_2}, {salmon_wpn_3}, {salmon_wpn_4}",
+                               value=f"ステージ: **{salmon_map}**",
+                               inline=False)
+        salmon_embed.set_thumbnail(url=f"{salmon_logo}")
+        salmon_embed.set_footer(text="API: https://spla2.yuu26.com")
+
+        await ctx.send(embed=salmon_embed)
 
 def setup(bot):
     bot.add_cog(Splatoon2(bot))
